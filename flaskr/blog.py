f033db0dda17a27a86e3be59748bf02f84716c6c
Germán Gutiérrez <me@sggc.me>
2018-11-17 01:19:08 -0600

    Edicion pitera completa

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -37,6 +37,8 @@ def create():
             errors.append('Se necesita el nombre.')
         if not genero:
             errors.append('Se necesita el genero.')
+        if not fechalanzamiento:
+            errors.append('Se necesita la fecha de lanzamiento.')
         if not desarrollador:
             errors.append('Se necesita el desarrollador.')
         if not clasificacion:
@@ -59,7 +61,6 @@ def create():
                 ' VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)',
                 (nombre, genero, fechalanzamiento, desarrollador, clasificacion, precio, rating, publicador, imagen, )
             )
-            print([nombre, genero, fechalanzamiento, desarrollador, clasificacion, precio, rating, publicador, imagen])
             commit_db()
             return redirect(url_for('blog.index'))
 
@@ -83,29 +84,52 @@ def get_juego(id, check_author=True):
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 def update(id):
-    post = get_juego(id)
+    juego = get_juego(id)
 
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
+        nombre = request.form['nombre']
+        genero = request.form['genero']
+        fechalanzamiento = request.form['fechalanzamiento']
+        desarrollador = request.form['desarrollador']
+        clasificacion = request.form['clasificacion']
+        precio = request.form['precio']
+        rating = request.form['rating']
+        publicador = request.form['publicador']
+        imagen = request.form['imagen']
+        errors = []
 
-        if not title:
-            error = 'Title is required.'
+        if not nombre:
+            errors.append('Se necesita el nombre.')
+        if not genero:
+            errors.append('Se necesita el genero.')
+        if not fechalanzamiento:
+            errors.append('Se necesita la fecha de lanzamiento.')
+        if not desarrollador:
+            errors.append('Se necesita el desarrollador.')
+        if not clasificacion:
+            errors.append('Se necesita la clasificacion.')
+        if not precio:
+            errors.append('Se necesita el precio.')
+        if not rating:
+            errors.append('Se necesita el rating.')
+        if not publicador:
+            errors.append('Se necesita el publicador.')
+        if not imagen:
+            imagen = None
 
-        if error is not None:
-            flash(error)
+        if len(errors) > 0:
+            flash('\n'.join(errors))
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = %s, body = %s'
-                ' WHERE id = %s',
-                (title, body, id)
+                'UPDATE juegos SET nombre = %s, genero = %s, fechalanzamiento = %s, desarrollador = %s, clasificacion = %s, precio = %s, rating = %s, publicador = %s, imagen = %s'
+                ' WHERE idjuego = %s',
+                (nombre, genero, fechalanzamiento, desarrollador, clasificacion, precio, rating, publicador, imagen, id)
             )
             commit_db()
             return redirect(url_for('blog.index'))
 
-    return render_template('blog/update.html', post=post)
+    return render_template('blog/update.html', juego=juego)
 
 
 @bp.route('/<int:id>/delete', methods=('POST',))
