e9fdd6115ffd8510a408b6d37d6d33021cdefcaf
Germán Gutiérrez <me@sggc.me>
2018-11-17 00:00:10 -0600

    Añadido cambio de tabla

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,8 +1,16 @@
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS juegos;
 
-CREATE TABLE post (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL
-);
+CREATE TABLE juegos (
+  idjuego int(11) NOT NULL AUTO_INCREMENT,
+  nombre varchar(50) NOT NULL,
+  genero varchar(50) NOT NULL,
+  fechalanzamiento date NOT NULL,
+  desarrollador varchar(50) NOT NULL,
+  clasificacion varchar(30) NOT NULL,
+  precio decimal(10,2) NOT NULL,
+  rating decimal(3,2) NOT NULL,
+  publicador varchar(50) NOT NULL,
+  imagen blob,
+  PRIMARY KEY (idjuego)
+) ;
+
